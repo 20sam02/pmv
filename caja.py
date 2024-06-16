f8b40cddcfa3ec9faf0d0b8bdd571942c3f07a45
20sam02 <saqv2002@gmail.com>
2024-06-16 00:30:14 -0500

    empezando

--- caja.py
+++ caja.py
@@ -1,5 +1,15 @@
-comidas = (
-)
-bebidas = (
+user = 'admin'
+pwd = '1234'
 
-)
+print("Ingresa el usuario y contraseña")
+
+print("usuario: ")
+user = input(str)
+print ("contraseña")
+pwd = input(int)
+
+if user == user and pwd == pwd :
+    print ("credenciales conrrectas")
+else:
+    print("usuario o contraseña incorrecta")
+    
